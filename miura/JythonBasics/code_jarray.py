9bf0114d91afdabfa470d744486fe1a8271c24e7
Kota Miura <kotapub@gmail.com>
2025-02-06 12:39:24 +0900

    typo fixed

--- miura/JythonBasics/code_jarray.py
+++ miura/JythonBasics/code_jarray.py
@@ -1,10 +1,10 @@
 import jarray
  
-#creat example data arrays
+#create example data arrays
 xa = [1, 2, 3, 4]
 ya = [3, 3.5, 4, 4.5]
  
-#convert to java array
+#convert to java arrays
 jxa = jarray.array(xa, 'i')
 jya = jarray.array(ya, 'd')
 print(jxa)
