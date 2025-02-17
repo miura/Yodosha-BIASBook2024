43f5ca3446d6c8b7010c161fcc23dbe771ef6b38
ToruHiratsuka <toru.hiratsuka@oici.jp>
2025-02-17 14:12:48 +0900

    Update Code13-NoFunction.py

--- hiratsuka/Code13-NoFunction.py
+++ hiratsuka/Code13-NoFunction.py
@@ -3,7 +3,7 @@
 #@ String (label = "File extension", value=".tif") ext
 #@ String (label = "File name contains", value = "") containString
 #@ Integer (label = "Background subtraction (Disabled if 0)", value = 50) BGval
-#@ Integer (Label = "3D filter (Disabled if 0)", value = 1) sigma
+#@ Integer (label = "3D filter (Disabled if 0)", value = 1) sigma
 
 #------------Trackmateのパラメータ-----------
 SegCh = 3
